4ef147ddd03e02332c6b2baeafee699bac53a939
Pasindu Sandeepa <38062478+PasinduSan@users.noreply.github.com>
2020-03-03 12:34:53 +0530

    error

---
+++ src/Component/Todos.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+
+class Todos extends React.Component() {
+    render(){
+    return (
+      <div >
+        <h1>Todos</h1>
+      
+      </div>
+    )
+  }
+  }
+ 
+
+export default Todos;
+ 
